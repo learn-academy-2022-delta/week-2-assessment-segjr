bdd3ffb95072bb02405cd3bfacaffee10026f87f
Segjr <samuel8016@gmail.com>
2022-06-27 03:44:48 -0700

    completed week 2 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -35,10 +35,36 @@ const num2 = 0
 const num3 = -7
 // Expected output: "-7 is not divisible by three"
 
+describe("byThree", () => {
+    it("takes in a number as an argument and returns if the number is divisible by three or not", () => {
+        expect(byThree(num1)).toEqual("15 is divisible by three")
+        expect(byThree(num2)).toEqual("0 is divisible by three")
+        expect(byThree(num3)).toEqual("-7 is not divisible by three")
+    })
+})
 
 // b) Create the function that makes the test pass.
 
+//Pseudo Code:
+// This program will allow the user to check if a number is divisible by three and return the result.
+// Create a function expression (byThree())
+// Create an if statement that evaluates if a given parameter is divisible by three
+// Have the function return the string interpolation `${number} is divisible by three
+/// Create an else if statement that evaluates if a given parameter is not divisible by three
+// Have the function return the string interpolation `${number} is not divisible by three
+// console log new function with challenge parameters
 
+let byThree = (number) => {
+    if(number % 3 == 0) {
+        return `${number} is divisible by three`
+    }else if(number % 3 !== 0) {
+        return `${number} is not divisible by three`
+    }
+}
+
+console.log(byThree(num1))
+console.log(byThree(num2))
+console.log(byThree(num3))
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
@@ -49,10 +75,34 @@ const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
+describe("upCase", () => {
+    it("takes in an array as an argument and returns an array with all strings capitalized", () => {
+        expect(upCase(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(upCase(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
 
 // b) Create the function that makes the test pass.
 
+// Pseudo Code:
+// This program will allow the user to input an array of strings and return the array with the strings capitalized.
+// Create a function expression. (upCase)
+// Utilize a method on the parameter to return a new array (.map())
+    // Within .map, utilize a method on the value parameter that will return a string object's index value. (charAt())
+    // Within .map, utilize a method on the aforementioned method that will convert the casing of a string object to uppercase letters. (toUpperCase())
+    // Within .map, utilize a method on a separated copy of the value parameter that will return the substring of a string by index value parameter. (.substr())
+    // Within .map, utilize an operator to merge and return the two value parameters for the .map value. (+)
+// Console log the new function with the challenge parameters. 
 
+
+let upCase = (object) => {
+    return object.map(x => {
+        return x.charAt(0).toUpperCase() + x.substr(1)
+    })
+}
+
+console.log(upCase(randomNouns1))
+console.log(upCase(randomNouns2))
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
@@ -65,5 +115,37 @@ const vowelTester2 = "academy"
 const vowelTester3 = "challenges"
 // Expected output: 2
 
+describe("theVowel", () => {
+    it("takes in a string as an argument and returns the index of the first vowel of the string", () => {
+        expect(theVowel(vowelTester1)).toEqual(1)
+        expect(theVowel(vowelTester2)).toEqual(0)
+        expect(theVowel(vowelTester3)).toEqual(2)
+    })
+})
 
 // b) Create the function that makes the test pass.
+
+// Pseudo Code:
+// This program will allow the user to enter a string and return the index of the first vowel of the string.
+// Create a function expression. (theVowel)
+// Create a variable that represents vowels. (v)
+// Create a for statement within the function, that will begin at the 0 index, end at the index length of a given string, and will count up by 1 increment.
+    // Within for, create a for statement that will begin at the 0 index, end at the index length of v, and will count up by 1 increment. 
+        // Within for, create an if statement that will evaluate if the given index value of a string is equivalent to an index value of the variable. 
+        // Have the function return the index of the given value if the evaluation is true. 
+// Console log the new function with the challenge parameters.
+
+let theVowel = (string) => {
+    let v = ["a","e","i","o","u"]
+    for(i = 0; i < string.length; i++) {
+        for(j = 0; j < v.length; j++) {
+            if(string[i] == v[j]) {
+            return i
+            }
+        }
+    }
+}
+
+console.log(theVowel(vowelTester1))
+console.log(theVowel(vowelTester2))
+console.log(theVowel(vowelTester3))
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,10 @@
 // --------------------1) What will this log?
 
 var cohort = "Delta 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ['D', 'e', 'l', 't', 'a', ' ', '2', '0', '2', '2']
+// b) Verify and explain: My initial answer was correct because the split method is used to split a string into a substring array. Using ("") as an arguement for the method separates the everything within double quotations. Since the variable is one string within double quotations, the return will split each index, including the space, into an array.
 
 
 // --------------------2) What will this log?
@@ -19,28 +19,28 @@ var cohort = "Delta 2022"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+ console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: syntax error
+// b) Verify and explain: My initial answer was incorrect because the function returned undefined. My intial thought was that there would be an error, due to there being no statement to execute the string interpolation. 
 
 
 // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [8, 10, 12, 14, 16]
+// b) Verify and explain: My initial answer is correct becase the .map method will make a new array from a given array, with the same index lenth. Since the value in .map method is being multiplied by 2, it will be applied to each index value of the array in ordance with the arrow function in the .map method. 
 
 
 // --------------------4) What will this log?
 
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain: My initial answer is correct because the .filter method will take an array and make a new array based on the evaluation passed through it. In this case, each value was evaluated to see if it was not evenly divisible by 2. If it was not, it would be returned in the new array. 
 
 
 // --------------------5) What will this log?
@@ -51,10 +51,10 @@ var myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "JavaScript"
+// b) Verify and explain: My initial answer is correct because the "." works as a path from the object (myCodingSkills) to one of the keys (languages), and the brackets indicate an index value that will be applied to said key. Since computers count from 0, the 0 index will be the first value of the key:value pair. That first value is JavaScript. 
 
 
 // --------------------STRETCH: What will this log?
@@ -67,7 +67,7 @@ class Learn {
   }
 }
 var learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Learn {student: "George", cohort: "Bravo", year: 2022}
+// b) Verify and explain: My initial answer is correct because the constructor method takes the given parameter and uses it as the name within the method when it returns the object created from the key:value pairs. The above method uses [this.] to reference the keys within the Learn class. The constructor method is used to create a new object. Since it is within the Learn class, Learn will be returned to associate the object with a class. Learn will work as a function to produce an object with the values assigned from the constructor keys. 
